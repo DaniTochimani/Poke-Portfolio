f034cb1a1ba02cc012df6fbbce6fea4c7e643f49
DaniTochimani <161892160+DaniTochimani@users.noreply.github.com>
2026-04-21 19:41:09 +0000

    Tue, Apr 21, 2026, 3:41 PM -04:00

--- game.py
+++ game.py
@@ -1,4 +1,14 @@
-'''
-game.py ties together 
+import market
+from player import Player
 
-'''
+STARTING_BALANCE = 500
+TOTAL_DAYS = 10
+DIVIDER = "-" * 55
+
+#Helpers
+def print_header(title: str) -> None:
+    print(f"\n{'=' * 55}")
+    print(f" {title}")
+    print(f"{'=' * 55}")
+
+def 
